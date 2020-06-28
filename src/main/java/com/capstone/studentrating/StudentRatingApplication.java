d788cb98c37bb2da8f351348be35307ea20d08e1
nathsagar96 <nathsagar96@gmail.com>
2020-06-28 18:09:25 +0530

    Initial commit

---
+++ src/main/java/com/capstone/studentrating/StudentRatingApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.studentrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/studentrating/StudentRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.studentrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
